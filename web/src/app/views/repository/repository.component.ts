ed0896550948c4eb0009ad7f0338b9ce70073c00
trippleflp <florian@gebele.net>
2020-04-30 17:35:40 +0200

    Initial frontend setup

---
+++ web/src/app/views/repository/repository.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repository',
+  templateUrl: './repository.component.html',
+  styleUrls: ['./repository.component.scss']
+})
+export class RepositoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ web/src/app/views/wizard/wizard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wizard',
+  templateUrl: './wizard.component.html',
+  styleUrls: ['./wizard.component.scss']
+})
+export class WizardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
