64951b7a6ef5c32f1ceca435729247c0403db084
whymatter <WHYm4tt3r.c0m>
2020-07-01 21:28:46 +0200

    update docker.compose

--- deployments/harbour/generate-key.sh
+++ deployments/harbour/generate-key.sh
@@ -3,7 +3,7 @@
 openssl req -nodes -newkey rsa:4096 \
   -keyout ./registry-auth.key \
   -out ./registry-auth.csr \
-  -subj "/CN=https:\/\/harbour.rocks"
+  -subj "/CN=https:\/\/io.harbour.rocks"
 
 openssl x509 \
   -in ./registry-auth.csr \
