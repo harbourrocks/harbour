9bd9465c97376206efcf00975579f4c255235b4b
whymatter <WHYm4tt3r.c0m>
2020-07-02 00:54:21 +0200

    a bit reformatting

--- web/src/app/views/builds/builds.component.ts
+++ web/src/app/views/builds/builds.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { GraphQlService } from 'src/app/services/graphql.service';
-import { Observable } from 'rxjs';
-import { List } from 'src/app/models/list.model';
-import { map, mergeAll } from 'rxjs/operators';
-import { BuildStatus } from 'src/app/models/build-status.enum';
+import {Component, OnInit} from '@angular/core';
+import {GraphQlService} from 'src/app/services/graphql.service';
+import {Observable} from 'rxjs';
+import {List} from 'src/app/models/list.model';
+import {map} from 'rxjs/operators';
+import {BuildStatus} from 'src/app/models/build-status.enum';
 
 @Component({
   selector: 'app-builds',
@@ -13,21 +13,23 @@ import { BuildStatus } from 'src/app/models/build-status.enum';
 export class BuildsComponent implements OnInit {
   builds: Observable<List>;
 
-  constructor(private graphQlService: GraphQlService) { }
+  constructor(private graphQlService: GraphQlService) {
+  }
 
   ngOnInit(): void {
     this.graphQlService.getAllBuilds().subscribe(console.log)
     this.builds = this.graphQlService.getAllBuilds()
-      .pipe(map(builds =>
-        ({
-          listItems: builds.map(build => ({
-            label: `${build.repository}:${build.tag}`,
-            preLabel: `#${build.buildId}`,
-            sufLabel: new Date(1593611348 * 1000).toISOString().substring(0, 10),
-            color: BuildStatus[build.buildStatus]
-
-          }))
-        })))
+      .pipe(
+        map(builds =>
+          ({
+            listItems: builds
+              .sort((a, b) => a?.startTime - b?.startTime)
+              .map(build => ({
+                label: `${build.repository}:${build.tag}`,
+                preLabel: `#${build.buildId.substr(0, 18)}`,
+                sufLabel: new Date(build.timestamp * 1000).toISOString(),
+                color: BuildStatus[build.buildStatus]
+              }))
+          })))
   }
-
 }
--- web/src/app/views/repository-details/repository-details.component.ts
+++ web/src/app/views/repository-details/repository-details.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { GraphQlService } from 'src/app/services/graphql.service';
-import { SimpleListItem } from 'src/app/models/simple-list-item.model';
-import { Tag } from 'src/app/models/graphql-models/tags.model';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { List } from 'src/app/models/list.model';
-import { BuildStatus } from 'src/app/models/build-status.enum';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {GraphQlService} from 'src/app/services/graphql.service';
+import {SimpleListItem} from 'src/app/models/simple-list-item.model';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {List} from 'src/app/models/list.model';
+import {BuildStatus} from 'src/app/models/build-status.enum';
 
 @Component({
   selector: 'app-repository-details',
@@ -21,7 +20,8 @@ export class RepositoryDetailsComponent implements OnInit {
 
   currentPageIndex = 0;
 
-  constructor(private route: ActivatedRoute, private graphQlService: GraphQlService) { }
+  constructor(private route: ActivatedRoute, private graphQlService: GraphQlService) {
+  }
 
   ngOnInit(): void {
     this.repositoryName = this.route.snapshot.paramMap.get('repo_name');
@@ -31,16 +31,15 @@ export class RepositoryDetailsComponent implements OnInit {
     this.builds = this.graphQlService.getRepositoryBuilds(this.repositoryName)
       .pipe(map(builds =>
         ({
-          listItems: builds.map(build => ({
-            label: `${build.repository}:${build.tag}`,
-            preLabel: `#${build.buildId}`,
-            sufLabel: new Date(1593611348 * 1000).toISOString().substring(0, 10),
-            color: BuildStatus[build.buildStatus]
-
-          }))
+          listItems: builds
+            .sort((a, b) => a?.startTime - b?.startTime)
+            .map(build => ({
+              label: `${build.repository}:${build.tag}`,
+              preLabel: `#${build.buildId.substr(0, 18)}`,
+              sufLabel: new Date(build.timestamp * 1000).toISOString(),
+              color: BuildStatus[build.buildStatus]
+            }))
         })))
-
-
   }
 
   pageChange(newIndex) {
