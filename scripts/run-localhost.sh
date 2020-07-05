95e253ac139f90053971d4374bfabc3409167c10
whymatter <WHYm4tt3r.c0m>
2020-07-05 16:07:13 +0200

    localhost deployment

--- scripts/run-localhost.sh
+++ scripts/run-localhost.sh
@@ -3,7 +3,7 @@
 openssl req -nodes -newkey rsa:4096 \
   -keyout ./deployments/localhost/registry-auth.key \
   -out ./deployments/localhost/registry-auth.csr \
-  -subj "/CN=http:\/\/localhost:5100"
+  -subj "/CN=http:\/\/harbour-iam:5100"
 
 openssl x509 \
   -in ./deployments/localhost/registry-auth.csr \
